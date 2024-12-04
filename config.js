becfc91d836a0dbb5d6972da5c1a0ecbb0c3f254
Ddinuwa <ddinuwa83@gmail.com>
2024-12-04 10:41:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
+    SESSION_ID: process.env.SESSION_ID || 'vuYHVJTD#Lu8ooCdpIr8bF-97wLbGBn4M2zxBCkRUn9jIOR8pXhU', // Enter Your Session ID
     MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '',    // Enter Your Owner Number
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '94767748006',    // Enter Your Owner Number
 };
